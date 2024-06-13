881c374321a21bcbbf8f38912f68e9ccae39d34e
Sluggcat <157683539+Sluggcat@users.noreply.github.com>
2024-06-13 11:36:09 +0200

    Update led_on_off.py

--- Programmes/Sluggcat/ESP32_tuto/led_on_off.py
+++ Programmes/Sluggcat/ESP32_tuto/led_on_off.py
@@ -1,11 +1,11 @@
 """
-    Standalone test : contrôle des GPIOS
+    Standalone test : basic GPIO output control
         Sluggcat
 """
 
-from machine import Pin # librairie pour contrôler les GPIO
+from machine import Pin # importation de la classe utile uniquement
 
 led = Pin(2, Pin.OUT) # déclaration du port lié à la LED
 
-led.value(0) # LED off
-# led.value(1) # LED on
+led.value(0) 		# LED of
+# led.value(1) 		# LED off
