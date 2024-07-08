b0081c5ad803261a166d29276ded800d837890f4
Sluggcat <157683539+Sluggcat@users.noreply.github.com>
2024-07-08 12:59:17 +0200

    add functionality
    send data over Serial

--- Programmes_C.R/TerraForma/datahandler.h
+++ Programmes_C.R/TerraForma/datahandler.h
@@ -1,16 +1,27 @@
 #ifndef datahandler_h
 #define datahandler_h
 
-#include "Arduino.h"
-#include "RTClib.h"  
+#include <Arduino.h>
+#include <RTClib.h>
 #include <SD.h>
 #include <SPI.h>
 
-#define SD_DEBUG 1 
+#define LOGGER_DEBUG 1
 #define SD_CS 10
 
 void Datalogger_setup(RTC_PCF8523 rtc);
 void read_RTC(RTC_PCF8523 rtc);
 
+class Measure_sender{
+  public:
+    Measure_sender(int baudRate, int nb_datas);
+    void begin();
+    void sendData(float* data);
+
+
+  private:
+    int baudRate, nb_datas;
+    void around(float* data);
+};
 
 #endif
